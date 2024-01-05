c540f2fe43be87d9e07aa9bf2ed3c831781341c8
Roman <romansk2001@gmail.com>
2024-01-05 15:00:41 +0200

    Update database configuration

--- config/db.php
+++ config/db.php
@@ -2,9 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=WD_BLOG_DB',
+    'password' => 'password',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
